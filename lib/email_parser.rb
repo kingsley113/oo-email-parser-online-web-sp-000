6c000a3780d21b6bfd92e913412471688be9c601
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-07-31 22:49:42 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -15,7 +15,8 @@ class EmailAddressParser
     # binding.pry
     # @emails = @emails.split(/\s | (,)/)
     @parsed_emails = @emails.split(/\s|(,)/)
-    @parsed_emails.delete("" || ",")
+    @parsed_emails.delete("")
+    @parsed_emails.delete(",")
     @parsed_emails
     binding.pry
   end
